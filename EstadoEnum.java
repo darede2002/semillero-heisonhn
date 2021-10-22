020e1e555dd808d3081f0133c0d6dbf139c3ca49
darede2002 <60675580+darede2002@users.noreply.github.com>
2021-10-22 00:07:15 -0500

    Entrega parcial
    Signed-off-by: darede2002 <60675580+darede2002@users.noreply.github.com>

---
+++ EstadoEnum.java
@@ -0,0 +1,17 @@
+/**
+ * EstadoEnum.java
+ */
+package com.hbt.semillero.enums;
+
+/**
+ * <b>Descripción:<b> Clase que determina el estado del comic
+ * <b>Caso de Uso:<b> semillero 2021
+ * @author Diego
+ * @version 
+ */
+public enum EstadoEnum {
+	ACTIVO("enum.estado.activo"),
+	INACTIVO("enum.estado.inactivo"),
+	;
+
+}
